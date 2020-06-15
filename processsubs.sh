ea8b668bb14bddec48444a94c91cc6b331beff8d
Peter Keel <git@discordia.ch>
2020-06-15 08:02:25 +0200

    processsubs: add indonesian and russian

--- processsubs.sh
+++ processsubs.sh
@@ -43,11 +43,13 @@ mmv '*_German.srt' '#1-ger.srt'  > /dev/null 2>&1
 mmv '*_Greek.srt' '#1-gre.srt'  > /dev/null 2>&1
 mmv '*_Hebrew.srt' '#1-heb.srt'  > /dev/null 2>&1
 mmv '*_Italian.srt' '#1-ita.srt'  > /dev/null 2>&1
+mmv '*_Indonesian.srt' '#1-ind.srt'  > /dev/null 2>&1
 mmv '*_Japanese.srt' '#1-jpn.srt'  > /dev/null 2>&1
 mmv '*_Korean.srt' '#1-kor.srt'  > /dev/null 2>&1
 mmv '*_Polish.srt' '#1-pol.srt'  > /dev/null 2>&1
 mmv '*_Portuguese.srt' '#1-por.srt'  > /dev/null 2>&1
 mmv '*_Romanian.srt' '#1-rum.srt'  > /dev/null 2>&1
+mmv '*_Russian.srt' '#1-rus.srt'  > /dev/null 2>&1
 mmv '*_Spanish.srt' '#1-spa.srt'  > /dev/null 2>&1
 mmv '*_Swedish.srt' '#1-swe.srt'  > /dev/null 2>&1
 mmv '*_Thai.srt' '#1-tha.srt'  > /dev/null 2>&1
